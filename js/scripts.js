5f05bb5fa475f1da9e65ae6b61890e2c63753937
Student <example@example.com>
2019-10-30 11:45:47 -0700

    commit before taking a break. Re-working code that was lost

--- js/scripts.js
+++ js/scripts.js
@@ -2,29 +2,27 @@
 const vowels = /^([^aeiouq])+/g;
 const consonants = /^([^bcdfghjklmnpqrstvxyz])+/g;
 const newArray = [];
-function pigLatin(sentenceArray, vowel, consonant, word){
-  sentenceArray.forEach(function(vowel){
-    if(sentenceArray.includes(vowel.charAt(0))){
-      newArray.push((vowel) + "way");
-      console.log("newArray: ", newArray);
-    } else if (sentenceArray.includes(consonant.charAt(0))){
-      newArray.push((sentenceArray.slice(1) + word.charAt(0)) + "ay");
-    } else{
-      alert("Nothing!")
+function pig(sentence, vowel, cons) {
+  let transformedArray = [];
+  sentence.forEach(function(word) {
+    if (vowel.includes(word.charAt(0))) {
+      transformedArray.push(word + "way")
+    } else if (cons.includes(word.charAt(0))) {
+      transformedArray.push(word.slice(1) + word.charAt(0) + "ay")
+    } else {
+      alert("nope")
     }
-  })
-}
+    });
+    return transformedArray;
+  };
 
-//Front-End Logic
-$(document).ready(function(){
-  $('#pigLatinForm').submit(function(event){
-  event.preventDefault();
-  const phraseInput = $('input#phraseInput').val();
-  let inputArray = phraseInput.split(" ");
-  // let secondSplitArray = inputArray.split(",");
-  console.log("Phrase: ", phraseInput);
-  console.log("Split Phrase: ", inputArray);
-  // console.log("Second Split: ", secondSplitArray);
-  pigLatin(inputArray,vowels, consonants);
+// user interface
+$(document).ready(function() {
+  $("form#userform").submit(function(event) {
+    event.preventDefault();
+    const input = $("input#userinput").val();
+    const inputArray = input.split(" ");
+    const thePig = pig(inputArray, vowels, consonants);
+    console.log("thePig: " + thePig);
   });
 });
