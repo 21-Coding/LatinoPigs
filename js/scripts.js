6fc0b2617ed284d06972fa5e23586c0d3d5c113a
Student <example@example.com>
2019-10-30 15:00:55 -0700

    add working Y check, vowel check, and consonant check

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,30 @@
 //Business Logic
-const vowels = /^([^aeiouq])+/g;
+const vowels = /^([aeiouq])+/g;
 // const vowels = ["a", "e", "i", "o","u"];
-const consonants = /^([^bcdfghjklmnpqrstvxyz])+/g;
+const consonants = /^([bcdfghjklmnpqrstvxyz])+/g;
 let newArray = [];
-// function pig(sentence, vowel, cons) {
-//   let transformedArray = [];
-//   console.log(sentence);
-//   sentence.forEach(function(word) {
-//     console.log(word.charAt(0));
-//     if (sentence.includes(word.charAt(0))) {
-//       transformedArray.push(word + "way")
-//       console.log("First if statement: ", transformedArray);
-//     } else if (sentence.includes(word.charAt(0))) {
-//       transformedArray.push(word.slice(1) + word.charAt(0) + "ay")
-//       console.log("2",transformedArray);
-//     } else {
-//       alert("Nothing")
-//       console.log("3", transformedArray);
-//     }
-//     });
-//     return transformedArray;
-//   };
 function pig(sentence, vowel, cons) {
   let transformedArray = [];
   console.log("1", sentence);
   sentence.forEach(function(word) {
-    console.log("1.5", word);
-    if(word.charAt(0).match(vowels)){
-      console.log("2", word);
-      console.log("Vowel: ", vowel);
-      transformedArray.push(word.charAt(0) + "way");
-      // word + way
-      console.log("3", word);
-    } else if(word.charAt(0).match(consonants)){
-        transformedArray.push(word.slice(1) + (word.charAt(0) + "ay"));
+    console.log("Word: ", word);
+    if(word.charAt(0) === "y"){ //If word has a Y
+      console.log("Char 0: ", (word.charAt(0)));
+      transformedArray.push(word.slice(1) +  word.charAt(0) + "ay");
+    } else if (word.charAt(0).match(cons)){ //If word has a vowel
+      console.log("Consonant Branch");
+      transformedArray.push(word + "way");
+    } else if(word.charAt(0).match(vowel)){ // If word has a consonant
+      console.log("Vowel Branch");
+        transformedArray.push(word + "way");
         console.log("4", sentence);
         console.log("5", word);
+    } else {
+      console.log("end");
     }
     });
-    console.log("4", sentence);
-    console.log("5", transformedArray);
+    console.log("6", sentence);
+    console.log("7", transformedArray);
     return transformedArray;
   };
 
