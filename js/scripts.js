aa54f0a4c723db95b1b59b0546862581c89ec811
Student <example@example.com>
2019-10-30 12:56:43 -0700

    add semi-functional first if/else. change CSS

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,37 @@
 const vowels = /^([^aeiouq])+/g;
 const consonants = /^([^bcdfghjklmnpqrstvxyz])+/g;
 let newArray = [];
+// function pig(sentence, vowel, cons) {
+//   let transformedArray = [];
+//   console.log(sentence);
+//   sentence.forEach(function(word) {
+//     console.log(word.charAt(0));
+//     if (sentence.includes(word.charAt(0))) {
+//       transformedArray.push(word + "way")
+//       console.log("First if statement: ", transformedArray);
+//     } else if (sentence.includes(word.charAt(0))) {
+//       transformedArray.push(word.slice(1) + word.charAt(0) + "ay")
+//       console.log("2",transformedArray);
+//     } else {
+//       alert("Nothing")
+//       console.log("3", transformedArray);
+//     }
+//     });
+//     return transformedArray;
+//   };
 function pig(sentence, vowel, cons) {
   let transformedArray = [];
-  console.log(sentence);
+  console.log("1", sentence);
   sentence.forEach(function(word) {
-    console.log(word.charAt(0));
-    if (sentence.includes(word.charAt(0))) {
-      transformedArray.push(word + "way")
-      console.log("First if statement: ", transformedArray);
-    } else if (sentence.includes(word.charAt(0))) {
-      transformedArray.push(word.slice(1) + word.charAt(0) + "ay")
-      console.log("2",transformedArray);
-    } else {
-      alert("Nothing")
-      console.log("3", transformedArray);
-    }
+    console.log("1.5", word);
+      if(word.includes(word.charAt(0))){
+        transformedArray.push(word.slice(1) + "ay");
+        console.log("2", sentence);
+        console.log("3", word);
+      }
     });
+    console.log("4", sentence);
+    console.log("5", transformedArray);
     return transformedArray;
   };
 
